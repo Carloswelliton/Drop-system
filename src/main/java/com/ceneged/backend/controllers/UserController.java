6194b0e62b46b3ac315bccc962078f52d3874f94
Carlos <carloswelliton7@gmail.com>
2025-08-13 22:01:37 -0400

    correção

--- src/main/java/com/ceneged/backend/controllers/UserController.java
+++ src/main/java/com/ceneged/backend/controllers/UserController.java
@@ -2,10 +2,8 @@ package com.ceneged.backend.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ceneged.backend.DTO.UserDTO;
 import com.ceneged.backend.models.User;
-import com.ceneged.backend.repository.UserRepository;
 import com.ceneged.backend.service.UserService;
 
 import jakarta.transaction.Transactional;
