b9e315a6f7bf8d9674ef352a79e5b6a0f146170f
Carlos <carloswelliton7@gmail.com>
2025-08-16 00:35:10 -0400

    **Spring Security com JWT para autenticação e autorização**  Finalizando a configuração do Token para que a rota de login esteja liberada e qualquer outra rota necessite doi token de validação para poder ser acessada, todas as validações ainda sendo feitas hardCoded via Insomnia

--- src/main/java/com/ceneged/backend/config/security/SecurityFilter.java
+++ src/main/java/com/ceneged/backend/config/security/SecurityFilter.java
@@ -4,9 +4,14 @@ package com.ceneged.backend.config.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.ceneged.backend.repository.UserRepository;
+
+import io.micrometer.common.lang.NonNull;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,7 +23,8 @@ public class SecurityFilter extends OncePerRequestFilter{
   @Autowired
   private TokenService tokenService;
 
-
+  @Autowired
+  private UserRepository repository;
 
   /*Filtro chamado logo após a requisição e antes do Spring MVC solicitar ao controller
   * ótimo para bloquear as requisições passando um filtro/condição especifica
@@ -29,21 +35,29 @@ public class SecurityFilter extends OncePerRequestFilter{
   throws ServletException, IOException {
 
     var tokenjWT = recuperarToken(request);
-    var subject = tokenService.getSubject(tokenjWT);
 
-
-     
-
+    if(tokenjWT != null){
+      //recuperando o token no cabeçalho
+      var subject = tokenService.getSubject(tokenjWT);
+      //recuperando o usuario
+      var usuario = repository.findByUsername(subject);
+      //carrega o usuario do bando de dados e força uma autenticação
+      var authentication = new UsernamePasswordAuthenticationToken(usuario, usuario.getAuthorities(), null);
+      //Aqui o Spring te considera logado
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+    
     filterChain.doFilter(request, response);
-
+    
   }
 
   // Bloqueando as requisições para que seeja necessário passar o Token
   private String recuperarToken(HttpServletRequest request) {
     var authorizationHeader = request.getHeader("Authorization");
-    if (authorizationHeader == null) {
-      throw new RuntimeException("Token JWT não enviado no cabeçalho authorization");
+    if (authorizationHeader != null) {
+      return authorizationHeader.replace("Bearer ","");
     }
-    return authorizationHeader.replace("Bearer ","");
+    return null;
+    
   }
 }
